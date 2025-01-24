f0048c2846c653204222e84661051572ed923091
Colin Boardway <cmboardway@gmail.com>
2025-01-24 12:17:23 -0600

    vercel build fix

--- api/dist/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { shortenUrl, redirectToUrl } from "./controllers/urlController.js";
-const router = Router();
-// Generate a short URL
-router.post("/shorten", shortenUrl);
-// Redirect to the long URL
-router.get("/:shortId", redirectToUrl);
-export default router;
