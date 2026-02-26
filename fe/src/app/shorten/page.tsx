210e27aded6db24072ae7fae28229656601066d0
Colin Boardway <cmboardway@gmail.com>
2026-02-26 12:17:21 -0600

    add test link for mlr express project

--- fe/src/app/shorten/page.tsx
+++ fe/src/app/shorten/page.tsx
@@ -4,8 +4,8 @@ export default function ShortenPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
-        <a href="/data-attribute" data-mlre="link"></a>
-        <a href="/no-data-attribute"></a>
+        <a href="/data-attribute" data-mlre="link">with data attribute</a>
+        <a href="/no-data-attribute">without data attribute</a>
         <ShortenForm />
       </div>
     </div>
