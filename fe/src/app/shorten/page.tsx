c3bbb8381d1c6bf3ee13ca66309ec684a3a84f37
Colin Boardway <cmboardway@gmail.com>
2026-03-03 12:11:06 -0600

    add test attributes

--- fe/src/app/shorten/page.tsx
+++ fe/src/app/shorten/page.tsx
@@ -5,33 +5,12 @@ export default function ShortenPage() {
     <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
       <div className="w-full max-w-sm md:max-w-3xl">
         {/* Links */}
-        <a href="/same-page">
-          anchor with data attribute
-        </a>
+        <a href="/same-page">anchor with data attribute</a>
 
         {/* Links with target _blank */}
-        <a
-          target="_blank"
-          href="https://www.google.com"
-        >
+        <a target="_blank" href="https://www.google.com">
           anchor with target _blank and data attribute
         </a>
-
-    
-        {/* Divs */}
-        <div
-          data-mlre-annotate-title="this is the div title"
-          data-mlre-annotate-text="this is the div text"
-        >
-          div with data attribute
-        </div>
-        <div>div without data attribute</div>
-
-        {/* Buttons */}
-        <button data-mlre-button="this is the button text">
-          button with data attribute
-        </button>
-        <button>button without data attribute</button>
 
         {/* Images */}
         <img
@@ -43,6 +22,24 @@ export default function ShortenPage() {
           src="https://w.wallhaven.cc/full/6l/wallhaven-6l5kp7.jpg"
           alt="image without data attribute"
         />
+
+        {/* Divs */}
+        <div
+          data-mlre-annotate-title="this is the div title"
+          data-mlre-annotate-text="this is the div text"
+        >
+          div with data attribute
+        </div>
+        <div>div without data attribute</div>
+
+        {/* Buttons */}
+        <button
+          data-mlre-annotate-title="this is the button title"
+          data-mlre-annotate-text="this is the button text"
+        >
+          button with data attribute
+        </button>
+        <button>button without data attribute</button>
 
         <ShortenForm />
       </div>
